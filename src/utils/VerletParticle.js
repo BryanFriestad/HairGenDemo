31dc25f5d1d2037b7c35932f7f0359cc3b3841e4
Zach Newton <znewton@iastate.edu>
2018-11-24 13:08:21 -0600

    Interpolate VerletParticles of ChildHairs

--- src/utils/VerletParticle.js
+++ src/utils/VerletParticle.js
@@ -1,4 +1,5 @@
 import { Vector3 } from 'lib/cuon-matrix';
+import Vector from 'utils/Vector';
 //a particle to represent a point in a hair strand simulated using verlet integration
 class VerletParticle {
   constructor(base_x, base_y, base_z, anchored, damping) {
@@ -8,33 +9,33 @@ class VerletParticle {
     this.acceleration = new Vector3();
     this.fixed_pos = anchored;
     this.dampen_factor = damping;
+    if (this.fixed_pos) this.acceloration = new Vector3();
+    else this.acceleration = new Vector3([0, -9.81, 0]);
   }
 
   update(delta_t) {
-    if (this.fixed_pos) {
-      this.prevPosition = this.position;
-      this.velocity = new Vector3();
-      this.acceleration = new Vector3();
-    } else {
-      this.acceleration = new Vector3([0, -9.81, 0]);
-      let prev_x = this.prevPosition.elements[0];
-      let prev_y = this.prevPosition.elements[1];
-      let prev_z = this.prevPosition.elements[2];
-      let x = this.position.elements[0];
-      let y = this.position.elements[1];
-      let z = this.position.elements[2];
-      let v_x = x - prev_x;
-      let v_y = y - prev_y;
-      let v_z = z - prev_z;
-      let a_x = this.acceleration.elements[0];
-      let a_y = this.acceleration.elements[1];
-      let a_z = this.acceleration.elements[2];
-      this.prevPosition = new Vector3([x, y, z]);
-      let new_x = x + v_x * this.dampen_factor + a_x * Math.pow(delta_t, 2);
-      let new_y = y + v_y * this.dampen_factor + a_y * Math.pow(delta_t, 2);
-      let new_z = z + v_z * this.dampen_factor + a_z * Math.pow(delta_t, 2);
-      this.position = new Vector3([new_x, new_y, new_z]);
-    }
+    if (this.fixed_pos) return;
+    let [prev_x, prev_y, prev_z] = [...this.prevPosition.elements];
+    let [x, y, z] = [...this.position.elements];
+    let [v_x, v_y, v_z] = [x - prev_x, y - prev_y, z - prev_z];
+    let [a_x, a_y, a_z] = this.acceleration.elements;
+    this.velocity = new Vector3([v_x, v_y, v_z]);
+    this.prevPosition = new Vector3([x, y, z]);
+    let new_x = x + v_x * this.dampen_factor + a_x * Math.pow(delta_t, 2);
+    let new_y = y + v_y * this.dampen_factor + a_y * Math.pow(delta_t, 2);
+    let new_z = z + v_z * this.dampen_factor + a_z * Math.pow(delta_t, 2);
+    this.position = new Vector3([new_x, new_y, new_z]);
+  }
+
+  interpolate({ weights: [b_A, b_B, b_C], parents: [p_A, p_B, p_C] }) {
+    if (this.fixed_pos) return;
+
+    let pA_pos = new Vector(p_A.position.elements).scale(b_A);
+    let pB_pos = new Vector(p_B.position.elements).scale(b_B);
+    let pC_pos = new Vector(p_C.position.elements).scale(b_C);
+
+    let new_pos = pA_pos.add(pB_pos).add(pC_pos);
+    this.position = new Vector3(new_pos.items);
   }
 
   setPosition(x, y, z) {
